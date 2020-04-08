c6421c918fbc49bfc22357d2053216b7c8e9ef41
Laithonex <laith.aljufaili966@gmail.com>
2020-04-09 03:33:47 +0400

    The complete Project <Be happy :)>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,20 @@
+# class HomeController < ApplicationController
+#   skip_before_action :authenticate_tenant!, :only => [ :index ]
+
+#   def index
+#     if current_user
+#       if session[:tenant_id]
+#         Tenant.set_current_tenant session[:tenant_id]
+#       else 
+#         Tenant.set_current_tenant current_user.tenants.first
+#       end 
+
+#       @tenant = Tenant.current_tenant
+#       @projects = Project.by_user_plan_and_tenant(@tenant.id, current_user)
+#       params[:tenant_id] = @tenant.id
+#     end 
+#   end
+# end
 class HomeController < ApplicationController
   skip_before_action :authenticate_tenant!, :only => [ :index ]
 
@@ -5,13 +22,13 @@ class HomeController < ApplicationController
     if current_user
       if session[:tenant_id]
         Tenant.set_current_tenant session[:tenant_id]
-      else 
+      else
         Tenant.set_current_tenant current_user.tenants.first
-      end 
-
+      end
+      
       @tenant = Tenant.current_tenant
       @projects = Project.by_user_plan_and_tenant(@tenant.id, current_user)
       params[:tenant_id] = @tenant.id
-    end 
+    end
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,28 +1,64 @@
-class Project < ApplicationRecord
-# class Project < ActiveRecord::Base
+# class Project < ApplicationRecord
+# # class Project < ActiveRecord::Base
 
+#   belongs_to :tenant
+#   # has_many :Artifacts, dependent: :destroy
+#   has_many :artifacts, dependent: :destroy
+#   has_many :user_projects
+#   has_many :users, through: :user_projects
+
+
+#   validates_uniqueness_of :title
+#   validate :free_plan_can_only_have_one_project
+
+#   def free_plan_can_only_have_one_project
+#     if self.new_record? && (tenant.projects.count < 0) && (tenant.plan == 'free')
+#       errors.add(:base,"free plans cannot have more than one project")
+#     end 
+#   end 
+
+#   def self.by_user_plan_and_tenant(tenant_id, user)
+#     tenant = Tenant.find(tenant_id)
+#     if tenant.plan == 'premium'
+#       if user.is_admin?
+#         tenant.projects
+#       else 
+#         user.projects.where(tenant_id: tenant.id)
+#       end
+#     else
+#       if user.is_admin?
+#         tenant.projects.order(:id).limit(1)
+#       else
+#         user.projects.where(tenant_id: tenant.id).order(:id).limit(1)
+#       end
+
+#       tenant.projects
+#     else
+#       tenant.projects.order(:id).limit(1)
+#     end 
+#   end 
+# end
+
+class Project < ActiveRecord::Base
   belongs_to :tenant
-  # has_many :Artifacts, dependent: :destroy
+  validates_uniqueness_of :title
   has_many :artifacts, dependent: :destroy
   has_many :user_projects
   has_many :users, through: :user_projects
-
-
-  validates_uniqueness_of :title
   validate :free_plan_can_only_have_one_project
-
+  
   def free_plan_can_only_have_one_project
-    if self.new_record? && (tenant.projects.count < 0) && (tenant.plan == 'free')
-      errors.add(:base,"free plans cannot have more than one project")
-    end 
-  end 
-
+    if self.new_record? && (tenant.projects.count > 0) && (tenant.plan == 'free')
+      errors.add(:base, "Free plans cannot have more than one project")
+    end
+  end
+  
   def self.by_user_plan_and_tenant(tenant_id, user)
     tenant = Tenant.find(tenant_id)
     if tenant.plan == 'premium'
       if user.is_admin?
         tenant.projects
-      else 
+      else
         user.projects.where(tenant_id: tenant.id)
       end
     else
@@ -31,10 +67,6 @@ class Project < ApplicationRecord
       else
         user.projects.where(tenant_id: tenant.id).order(:id).limit(1)
       end
-
-      tenant.projects
-    else
-      tenant.projects.order(:id).limit(1)
-    end 
-  end 
+    end
+  end
 end
